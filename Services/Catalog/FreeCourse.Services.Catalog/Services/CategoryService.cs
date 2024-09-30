42844f0749fa9d72ec6b359d454e7d9d834351b6
Mustafa TORTUK <mustafat237@hotmail.com>
2024-09-30 20:29:21 +0300

    Category service added.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/CategoryService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Catalog.DTO;
+using FreeCourse.Services.Catalog.Models;
+using FreeCourse.Services.Catalog.Settings;
+using FreeCourse.Shared.DTO;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IMongoCollection<Category> _categoryCollection;
+
+        private readonly IMapper _mapper;
+
+        public CategoryService(IMapper mapper, IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+
+            _categoryCollection = database.GetCollection<Category>(databaseSettings.CategoryCollectionName);
+            _mapper = mapper;
+        }
+
+        public async Task<ResponseDTO<List<CategoryDTO>>> GetAll()
+        {
+            var categories = await _categoryCollection.Find(category => true).ToListAsync();
+            return ResponseDTO<List<CategoryDTO>>.Success(_mapper.Map<List<CategoryDTO>>(categories), 200);
+        }
+
+        public async Task<ResponseDTO<CategoryDTO>> CreateAsync(Category category)
+        {
+            await _categoryCollection.InsertOneAsync(category);
+
+            return ResponseDTO<CategoryDTO>.Success(_mapper.Map<CategoryDTO>(category),200);
+            //return ResponseDTO<CategoryDTO>.Success(200);
+        }
+
+        public async Task<ResponseDTO<CategoryDTO>> GetByIdAsync(string id)
+        {
+            var category = await _categoryCollection.Find(x=> x.Id == id).FirstOrDefaultAsync();
+
+            if (category == null)
+            {
+                return ResponseDTO<CategoryDTO>.Fail("Record not found!",404);
+            }
+            return ResponseDTO<CategoryDTO>.Success(_mapper.Map<CategoryDTO>(category),200);
+        }
+    }
+}
---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using FreeCourse.Services.Catalog.DTO;
+using FreeCourse.Services.Catalog.Models;
+using FreeCourse.Shared.DTO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+    public interface ICategoryService
+    {
+        Task<ResponseDTO<List<CategoryDTO>>> GetAll();
+        Task<ResponseDTO<CategoryDTO>> CreateAsync(Category category);
+        Task<ResponseDTO<CategoryDTO>> GetByIdAsync(string id);
+    }
+}
