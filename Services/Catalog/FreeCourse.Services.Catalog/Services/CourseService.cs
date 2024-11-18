031b144b5ba1990471ba676ba6bc5a6f1ede6072
Mustafa TORTUK <mustafat237@hotmail.com>
2024-11-19 00:13:41 +0300

    Course service edited.

--- Services/Catalog/FreeCourse.Services.Catalog/Services/CourseService.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/CourseService.cs
@@ -36,7 +36,7 @@ namespace FreeCourse.Services.Catalog.Services
             {
                 foreach (var course in courses)
                 {
-                    course.Category = await _categoryCollection.Find(x => x.Id == course.CategoryId).FirstAsync();
+                    course.Category = await _categoryCollection.Find<Category>(x => x.Id == course.CategoryId).FirstAsync();
                 }
             }
             else
@@ -45,6 +45,39 @@ namespace FreeCourse.Services.Catalog.Services
             }
 
             return ResponseDTO<List<CourseDTO>>.Success(_mapper.Map<List<CourseDTO>>(courses),200);
+        }
+
+        public async Task<ResponseDTO<CourseDTO>> GetByIdAsync(string id) 
+        {
+            var course = await _courseCollection.Find<Course>(x => x.Id == id).FirstOrDefaultAsync(); //Arama yapılacağı zaman model belirtilir
+
+            if (course == null)
+            {
+                return ResponseDTO<CourseDTO>.Fail("Data not found", 404);
+            }
+
+            course.Category = await _categoryCollection.Find<Category>(x => x.Id == course.CategoryId).FirstOrDefaultAsync();
+
+            return ResponseDTO<CourseDTO>.Success(_mapper.Map<CourseDTO>(course), 200); //DTO - model dönüşümü
+        }
+
+        public async Task<ResponseDTO<List<CourseDTO>>> GetByUserId(string id)
+        {
+            var courses = await _courseCollection.Find<Course>(x => x.UserId == id).ToListAsync();
+
+            if(courses.Any())
+            {
+                foreach (var course in courses)
+                {
+                    course.Category = await _categoryCollection.Find<Category>(c => c.Id == course.CategoryId).FirstAsync();
+                }
+            }
+            else
+            {
+                courses = new List<Course>();
+            }
+
+            return ResponseDTO<List<CourseDTO>>.Success(_mapper.Map<List<CourseDTO>>(courses), 200);
         }
     }
 }
